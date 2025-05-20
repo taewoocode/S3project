c2750c5dc8d605af4a173241eda0b7da9aad01ac
taewoocode <taewoocode@gmail.com>
2025-05-21 05:04:23 +0900

    BT-1[refactor]: JPA로 User 엔티티 리팩터링

---
+++ src/main/java/com/example/s3test/model/User.java
@@ -0,0 +1,43 @@
+package com.example.s3test.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, columnDefinition = "VARCHAR(255) COMMENT '사용자 아이디'")
+    private String userId;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) COMMENT '사용자 비밀번호'")
+    private String userPw;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) COMMENT '사용자 이메일'")
+    private String userEmail;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) COMMENT '사용자 이름'")
+    private String userName;
+
+    @Builder
+    public User(String userID, String userPW, String userEmail, String userName) {
+        this.userId = userID;
+        this.userPw = userPW;
+        this.userEmail = userEmail;
+        this.userName = userName;
+    }
+}
